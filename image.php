d2f9695b1e4d7f67f218b4b7b256be7f55573822
quyleanh <quyleanh121093@gmail.com>
2017-04-19 22:13:02 +0700

    update owl and background options

--- image.php
+++ image.php
@@ -3,8 +3,76 @@
 <head>
 	<title>Your owl is here...</title>
 	<link rel="stylesheet" href="styles/styles.css">
+	<style>
+		
+	</style>
 </head>
 <body>
+	<?php 
+
+	function mergeImages($filename_src, $filename_dest, $filename_result) {
+ 		// Get dimensions for specified images
+ 		// _src is foreground, _dest is background
+		list($width_src, $height_src) = getimagesize($filename_src);	
+		list($width_dest, $height_dest) = getimagesize($filename_dest);
+ 		// Create new image with desired dimensions (background image)
+		$image = imagecreatetruecolor($width_dest, $height_dest);
+ 		// Load images and then copy to destination image
+		$image_x = imagecreatefrompng($filename_src);
+		$image_y = imagecreatefrompng($filename_dest);
+		imagecopy($image, $image_x, 0, 0, 0, 0, $width_dest, $height_dest);
+		imagecopy($image, $image_y, 0, 0, 0, 0, $width_dest, $height_dest);
+ 		// Save the resulting image to disk (as PNG)
+		imagepng($image, $filename_result);
+		// Clean up
+		imagedestroy($image);
+		imagedestroy($image_x);
+		imagedestroy($image_y);
+	}
+
+	function getOptions($owlOption = 'a', $bgOption = '1') {
+		switch ($owlOption) {
+			case 'option1':
+				$owl = 'a';
+				break;
+			case 'option2':
+				$owl = 'b';
+				break;
+			case 'option3':
+				$owl = 'c';
+				break;
+			case 'option4':
+				$owl = 'd';
+				break;
+			default:
+				$owl = 'a';
+			break;
+		}
+		switch ($bgOption) {
+			case 'option1':
+				$bg = '1';
+				break;
+			case 'option2':
+				$bg = '2';
+				break;
+			case 'option3':
+				$bg = '3';
+				break;
+			case 'option4':
+				$bg = '4';
+				break;				
+			case 'option5':
+				$bg = '5';
+				break;
+			default:
+				$bg = '6';
+				break;
+		}
+		return array ($owl, $bg);
+	}
+
+	?>
+
 	<?php
 	date_default_timezone_set("Asia/Ho_Chi_Minh");
 	function generateRandomString($length = 10) {
@@ -21,10 +89,15 @@
 	$result = str_replace(" ", "_", $result);
 	$path = "owls/" . generateRandomString(4) . "_" . $result . ".png";
 
-	$bg = 'errol.png';
+	list($foreGround, $backGround) = getOptions($_POST["optionsOwls"], $_POST["optionsBackgrounds"]);
+	
+	// fg[number]_bg[number]
+	// Check the existent image
+	$letterImage = "images/" . $foreGround . $backGround . ".png";
+
 	$image = new PHPImage();
-	$image->setDimensionsFromImage($bg);
-	$image->draw($bg);
+	$image->setDimensionsFromImage($letterImage);
+	$image->draw($letterImage);
 
 	$fontPath = './HONEY-CREAM.ttf';
 	$image->setFont($fontPath);
@@ -38,8 +111,10 @@
 		));
 	$image->save($path, false, true);
 	?>
-	<div class="fill">
-		<img class="image" src="<?php echo $path?>" />		
+	<div id="loadingbar-frame" class="fill">	
+		<a href="<?php echo $path?>" download>
+			<img class="image" src="<?php echo $path?>" alt="#sendyourowl">
+		</a>
 	</div>
 </body>
 </html>
--- sendyourowl.php
+++ sendyourowl.php
@@ -12,6 +12,8 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<style>
 		body {
 			width: 100%;
@@ -21,18 +23,18 @@
 	</style>
 </head>
 <body>
-<div style="width: 77%; margin: auto;" class="container-fluid">
+	<div style="width: 77%; margin: auto;" class="container-fluid">
 		<div class="bg-3 text-center">
 			<h3><b>Học Viện Ma Thuật và Pháp Thuật Hogwarts Việt Nam</b></h3>
-			<img src="logo.png" class="img-circle" width="30%" height="30%" alt="Bird">
+			<img src="images/logo.png" class="img-circle" width="30%" height="30%" alt="Bird">
 			<h3>#sendyourowl</h3>
 		</div>
 
-		<form action="image.php" method="post" role="form">
+		<form class="ajax-call" action="image.php" method="post" role="form">
 			<div class="form-group">
 				<label for="name" class="col-sm-2 control-label">Enter your owl:</label>
 				<div class="col-sm-10">
-					<textarea class="form-control" name="yourowl" rows="5" placeholder="Your letter should be in 200 words"></textarea><br />
+					<textarea class="form-control" name="yourowl" rows="5" placeholder="Your letter should be in 200 characters" maxlength="200"></textarea><br />
 				</div>
 			</div>
 
@@ -41,19 +43,19 @@
 				<div class="col-sm-10">
 					<label>
 						<input type="radio" name="optionsOwls" id="optionsRadios1" value="option1" checked>
-						Hedwig
+						Hedwig trắng muốt
 					</label><br>
 					<label>
 						<input type="radio" name="optionsOwls" id="optionsRadios2" value="option2">
-						Pigwidgeon
+						Heo nhí loi choi
 					</label><br>
 					<label>
-						<input type="radio" name="optionsOwls" id="optionsRadios2" value="option3">
-						Hermes
+						<input type="radio" name="optionsOwls" id="optionsRadios3" value="option3">
+						Errol hiền hậu
 					</label><br>
 					<label>
-						<input type="radio" name="optionsOwls" id="optionsRadios2" value="option4">
-						Errol
+						<input type="radio" name="optionsOwls" id="optionsRadios4" value="option4">
+						Hermes ngạo nghễ
 					</label><br><br>
 				</div>
 			</div>
@@ -63,15 +65,23 @@
 				<div class="col-sm-10">
 					<label>
 						<input type="radio" name="optionsBackgrounds" id="optionsRadios1" value="option1" checked>
-						Background 1
+						Lâu đài Hogwarts
 					</label><br>
 					<label>
 						<input type="radio" name="optionsBackgrounds" id="optionsRadios2" value="option2">
-						Background 2
+						Trang trại Hang Sóc
 					</label><br>
 					<label>
-						<input type="radio" name="optionsBackgrounds" id="optionsRadios2" value="option3">
-						Background 3
+						<input type="radio" name="optionsBackgrounds" id="optionsRadios3" value="option3">
+						Privet Drive
+					</label><br>
+					<label>
+						<input type="radio" name="optionsBackgrounds" id="optionsRadios4" value="option4">
+						Quidditch chiều hoàng hôn
+					</label><br>
+					<label>
+						<input type="radio" name="optionsBackgrounds" id="optionsRadios5" value="option5">
+						Quidditch ngày đẹp trời
 					</label><br><br>
 				</div>
 			</div>
